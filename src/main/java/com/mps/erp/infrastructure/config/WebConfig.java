0b7367504f240d7547ddb13ef77db3c9faba3d6c
Manso-Humilde <edgar.morel.fp@gmail.com>
2026-05-31 20:29:54 +0200

    Fix CORS for Vercel

--- src/main/java/com/mps/erp/infrastructure/config/WebConfig.java
+++ src/main/java/com/mps/erp/infrastructure/config/WebConfig.java
@@ -20,10 +20,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("https://mps-erp.vercel.app", "http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 
     @Override
